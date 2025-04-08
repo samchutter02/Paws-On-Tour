294579f0ddcc3514b170ff27724a46d9f06dd392
Sam C <samcwebdev@gmail.com>
2025-04-08 12:43:24 -0500

    Add files via upload

---
+++ Paws_On_TourApp.swift
@@ -0,0 +1,19 @@
+//
+//  Paws_On_TourApp.swift
+//  Paws On Tour
+//
+//  Created by Sam Chutter on 4/7/25.
+//
+
+
+// PawsOnTourApp.swift
+import SwiftUI
+
+@main
+struct PawsOnTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
